66b3ddd630c6b21f4b0a4c48057480860bdb369f
RyotaFukumoto <f.ryota.828@gmail.com>
2023-09-09 22:06:02 +0900

    add cats

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create() {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll() {
+    return 'This action returns all cats';
+  }
+}
