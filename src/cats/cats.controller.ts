9a657340879e4c88a048f3c58d4b197c78f7ac6d
RyotaFukumoto <f.ryota.828@gmail.com>
2023-09-13 22:47:40 +0900

    initial commit

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create() {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll() {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
